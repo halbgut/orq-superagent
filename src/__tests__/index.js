394d8791baa91984ae75fa6d87f864a1c522b731
Luca Nils Schmid <me@lucaschmid.net>
2017-05-21 21:30:46 +0200

    feat: implement initial version + tests

---
+++ src/__tests__/index.js
@@ -0,0 +1,101 @@
+import { Observable as O } from 'rxjs'
+import test from 'ava'
+import nock from 'nock'
+
+import request from '../'
+
+test('request should make a GET request by default', t => {
+  t.plan(3)
+  const url = 'https://1.example.com'
+  nock(url)
+    .get('/')
+    .reply(200, 'YOLO')
+  return request(url)
+    .do(res => {
+      t.is(res.status, 200)
+      t.is(res.body, 'YOLO')
+      t.is(typeof res.headers, 'object')
+    })
+})
+
+test('request should send headers', t => {
+  t.plan(3)
+  const url = 'https://2.example.com'
+  nock(url, {
+    reqheaders: {
+      'X-Yolo': 'IMMER!',
+    }
+  })
+    .get('/')
+    .reply(200, 'YOLO')
+  return request(url, { headers: { 'X-Yolo': 'IMMER!' } })
+    .do(res => {
+      t.is(res.status, 200)
+      t.is(res.body, 'YOLO')
+      t.is(typeof res.headers, 'object')
+    })
+})
+
+test('request should pass the response headers to the subscriber', t => {
+  t.plan(1)
+  const url = 'https://3.example.com'
+  nock(url)
+    .get('/')
+    .reply(200, 'YOLO', { 'X-Yolo': 'Manchmal...' })
+  return request(url)
+    .do(res => {
+      t.is(res.headers['x-yolo'], 'Manchmal...')
+    })
+})
+
+test('request should throw if the response status code is non 2xx', t => {
+  t.plan(1)
+  const url = 'https://4.example.com'
+  nock(url)
+    .get('/')
+    .reply(500, 'ERROR!')
+  return request(url)
+    .catch(err => {
+      t.is(err.response.body, 'ERROR!')
+      return O.empty()
+    })
+})
+
+// XXX: this test doesn't actually test if the HTTP request is canceled.
+test.cb('request should cancel the request if the observable is unsubscribed from.', t => {
+  const url = 'https://5.example.com'
+  nock(url)
+    .get('/')
+    .delay(100)
+    .reply(200, '123')
+  const sub = request(url)
+    .do((res) => {
+      t.fail()
+    })
+    .subscribe()
+  setTimeout(() => {
+    sub.unsubscribe()
+    t.pass()
+    t.end()
+  }, 50)
+})
+
+test('request should use the specified method', t => {
+  t.plan(1)
+  const url = 'https://4.example.com'
+  nock(url)
+    .post('/')
+    .reply(200, 'TROLOLO')
+  return request(url, { method: 'POST' })
+    .do((res) => { t.is(res.body, 'TROLOLO') })
+})
+
+test('request should send the specified body along with the request', t => {
+  t.plan(1)
+  const url = 'https://4.example.com'
+  nock(url)
+    .post('/', { x: true })
+    .reply(200, 'TROLOLO')
+  return request(url, { method: 'POST', body: { x: true } })
+    .do((res) => { t.is(res.body, 'TROLOLO') })
+})
--- src/index.js
+++ src/index.js
@@ -9,7 +9,45 @@ type RequestOptions = {
   body?: any,
 }
 
+const defaults = {
+  method: 'GET',
+}
+
 const request = (url: string, options: ?RequestOptions):
-  rxjs$Observable<any> => {}
+  rxjs$Observable<any> =>
+    O.create(o => {
+      const { headers, method, body } = Object.assign({}, defaults, options)
+      const request = superagent(method, url)
+      if (headers) request.set(headers)
+      if (body) request.send(body)
+      request.end((err, res) => {
+        if (err) return o.error(new HttpRequestError(res))
+        o.next(getResponseProps(res))
+        o.complete()
+      })
+      return () => {
+        if (!request.response) {
+          request.abort()
+        }
+      }
+    })
+
+const getResponseProps = res => {
+  const body = Object.keys(res.body).length
+    ? res.body
+    : res.text
+  return {
+    status: res.status,
+    headers: res.headers,
+    body,
+  }
+}
+
+function HttpRequestError(res) {
+  this.message = 'HTTP request failed.'
+  this.response = getResponseProps(res)
+}
+
+(HttpRequestError: any).prototype = Error.prototype
 
 export default request
